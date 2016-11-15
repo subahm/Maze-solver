f820f68c66808aebea01b50375be76e9360c548d
Subah Mehrotra <subahmehrotra@gmail.com>
2016-11-15 13:01:53 -0800

    Update RunSolver.java

--- RunSolver.java
+++ RunSolver.java
@@ -1,19 +1,7 @@
-/**
- * RunSolver.java
- *
- * Created for CSC115 201605 Assignment four.
- * Modified from a previous version created by M. Zastre.
- * Current modification: B. Bultena
- *
- * This top-level program takes the name of a file containing maze
+/* This top-level program takes the name of a file containing maze
  * information, and then reads this into a integers and an array
  * of strings. The Maze object is instantiated, solve() called on it
  * and the results (if any) are output on the console.
- *
- * This code will not compile until there is an implementation of
- * the Maze.java class that has at least a constructor and a
- * bare-bones implementation of solve() (i.e., bare-bones means that
- * the method simply returns null).
  */
 
 import java.io.File;
@@ -24,21 +12,11 @@ public class RunSolver {
 
 	/**
 	 * Runs an external test of the Maze class.
-	 * @param args The single name of a text file containing maze information
-	 *	in the following format (excluding the line number):
-	 * 	<ol>
-	 *	<li> int int</li>
-	 *	<li> int int</li>
-	 *	<li> int int</li>
-	 *	<li> maze row of spaces and asterisks.</li>
-	 * 	<li> repeat similar lines as many times as required.</li>
-	 *	</ol>
 	 * The first line contains the number of rows and the number of columns.
 	 * The second line contains the cell number of the entrance to the maze.
 	 * The third line contains the cell number of the exit from the maze.
-	 * Subsequent lines contain equal length strings of asterisks and spaces,
-	 *	an asterisk representing a wall and a space representing a free cell.
  	 */
+	
 	public static void main(String args[]) {
 		if (args.length < 1) {
 			System.err.println("usage: java RunSolver <mazefile");
